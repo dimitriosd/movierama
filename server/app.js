d9933b7023c691f1c5ba0dcb819057589122726a
dimitriosdouras <dimitrios.n.douras@gmail.com>
2019-08-23 13:15:19 +0300

    Add heroku

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 require('./db/mongoose')
+const path = require('path');
 
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/movie')
@@ -16,5 +17,14 @@ app.use(reviewsRouter);
 app.use(bodyParser.json({ type: '*/*' }));
 app.use(morgan('combined'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 module.exports = app
