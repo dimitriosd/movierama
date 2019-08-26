62356d9a7cfcac2e88188b151ee1ec163b4ffb6a
dimitriosdouras <dimitrios.n.douras@gmail.com>
2019-08-26 16:01:20 +0300

    Remove unused routes

--- server/routers/movie.js
+++ server/routers/movie.js
@@ -98,42 +98,5 @@ router.get('/api/movies/:userId', auth, async (req, res) => {
   }
 });
 
-router.get('/api/movie/:id', auth, async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const movie = await Movie.findOne({ _id, owner: req.user._id });
-    if (!movie) {
-      res.status(404).send();
-    }
-    res.send(movie);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-router.patch('/api/movies/:id', auth, async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ['description', 'completed'];
-
-  const isValidOperation = updates.every(update => allowedUpdates.includes(update));
-  if (!isValidOperation) {
-    res.status(400).send({ error: 'Invalid updates' });
-  }
-
-  try {
-    const movie = await Movie.findOne({ _id: req.params.id, owner: req.user._id });
-    if (!movie) {
-      return res.status(404).send();
-    }
-
-    updates.forEach(update => movie[update] = req.body[update]);
-    await movie.save();
-
-    res.send(movie);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
 
 module.exports = router;
